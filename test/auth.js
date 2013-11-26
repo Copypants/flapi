15626a0ca2a3dff84b8a43ea84b38d2d6e7f857c
joelongstreet <joelongstreet@gmail.com>
2013-11-26 09:35:08 -0600

    writing a failing access token test

--- test/auth.js
+++ test/auth.js
@@ -1,8 +1,12 @@
-var assert  = require('assert');
-var should  = require('should');
-var flickr  = require('../index');
+var assert    = require('assert');
+var should    = require('should');
+var flickr    = require('../index');
+var authUtils = require('./authUtils');
+var port      = 3001;
+
 
 describe('authorization', function(){
+
 
   before(function(){
     this.flickrClient = new flickr({
@@ -19,8 +23,9 @@ describe('authorization', function(){
 
   it('should reach out to flickr and receive an oauth_token and an oauth_token_secret', function(done){
     var self = this;
+    var callbackURL = 'http://localhost:' + port + '/auth_callback';
 
-    this.flickrClient.authApp('http://localhost:3000', function(settings){
+    this.flickrClient.authApp(callbackURL, function(settings){
       settings.should.have.properties('oauth_callback_confirmed', 'oauth_token', 'oauth_token_secret');
       settings.oauth_callback_confirmed.should.equal('true');
 
@@ -30,12 +35,33 @@ describe('authorization', function(){
 
 
   it('should return a user authorization URL when prompted', function(){
+    this.timeout(5000);
+
     var url   = this.flickrClient.getUserAuthURL();
     var query = url.split('?')[1];
     var token = query.split('=')[1];
-
     token.should.not.equal('undefined');
     token.length.should.be.above(10);
+
+    // Make the auth url available so we can later verify it works
+    this.authURL = url;
+  });
+
+
+  it('should be able to fetch a user access token', function(done){
+    this.timeout(30000);
+    var self = this;
+
+    var matched = function(queryParams){
+      self.flickrClient.getUserAccessToken(queryParams.oauth_verifier, function(accessToken){
+        accessToken.should.not.equal('undefined');
+        accessToken.length.should.be.above(10);
+        done();
+      });
+    };
+
+    authUtils.createRouteListener('/auth_callback', matched);
+    authUtils.simulateUserApproval(this.flickrClient.getUserAuthURL());
   });
 
 });
