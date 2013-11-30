c94a19e4ae7da215e5c3300592517d62527bf4ad
joelongstreet <joelongstreet@gmail.com>
2013-11-30 09:47:15 -0600

    permissions can now be set when initting the app

--- test/client.js
+++ test/client.js
@@ -1,8 +1,9 @@
 var should        = require('should');
 var flickr        = require('../index');
 var flickrClient  = new flickr({
-  oauth_consumer_key     : process.env.FLICKR_KEY,
-  oauth_consumer_secret  : process.env.FLICKR_SECRET
+  oauth_consumer_key    : process.env.FLICKR_KEY,
+  oauth_consumer_secret : process.env.FLICKR_SECRET,
+  perms                 : 'delete'
 });
 
 
