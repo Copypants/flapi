53a33dd4a60263f225aa3520686e4e20dc127d4a
joelongstreet <joelongstreet@gmail.com>
2013-12-02 10:09:04 -0600

    whoops, remove garbage

--- test/api-common.js
+++ test/api-common.js
@@ -1,15 +1,14 @@
 var should        = require('should');
 var flickrClient  = require('./client').client;
-var photoData     = {};
 
 describe('authorized api', function(){
   this.timeout(10000);
 
   it('should be able to fetch a list of camera brand models', function(done){
     flickrClient.api({
-      method      : 'flickr.cameras.getBrandModels',
-      params      : { brand : 'apple' },
-      next        : function(data){
+      method : 'flickr.cameras.getBrandModels',
+      params : { brand : 'apple' },
+      next   : function(data){
         data.should.have.properties('cameras', 'stat');
         data.stat.should.equal('ok');
         data.cameras.should.have.property('camera');
@@ -21,8 +20,8 @@ describe('authorized api', function(){
 
   it('should be able to fetch a list of interesting photos', function(done){
     flickrClient.api({
-      method      : 'flickr.interestingness.getList',
-      next        : function(data){
+      method : 'flickr.interestingness.getList',
+      next   : function(data){
         data.should.have.properties('photos', 'stat');
         data.stat.should.equal('ok');
         data.photos.should.have.property('photo');
