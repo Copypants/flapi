381a2db358635b53363fec7ee1c090253262385c
joelongstreet <joelongstreet@gmail.com>
2013-12-08 10:36:02 -0600

    add a way to test without moving through entire authorization flow

--- test/auth.js
+++ test/auth.js
@@ -1,65 +1,75 @@
 var should        = require('should');
 var flickr        = require('../index');
 var authUtils     = require('./authUtils');
+var cli           = require('./cli');
 var flickrClient  = require('./client').client;
 var port          = 3001;
 var userToken     = {};
 
 
-describe('authorization', function(){
+if(cli.useAuth){
 
+  describe('authorization', function(){
 
-  it('should throw an error if no auth callback is defined', function(){
-    flickrClient.authApp.should.throw()
-  });
-
-
-  it('should reach out to flickr and receive an oauth_token and an oauth_token_secret', function(done){
-    var callbackURL = 'http://localhost:' + port + '/auth_callback';
-
-    flickrClient.authApp(callbackURL, function(settings){
-      settings.should.have.properties('oauth_callback_confirmed', 'oauth_token', 'oauth_token_secret');
-      settings.oauth_callback_confirmed.should.equal('true');
-
-      done();
+    it('should throw an error if no auth callback is defined', function(){
+      flickrClient.authApp.should.throw()
     });
-  });
 
 
-  it('should return a user authorization URL when prompted', function(){
-    this.timeout(5000);
+    it('should reach out to flickr and receive an oauth_token and an oauth_token_secret', function(done){
+      var callbackURL = 'http://localhost:' + port + '/auth_callback';
 
-    var url   = flickrClient.getUserAuthURL();
-    var query = url.split('?')[1];
-    var token = query.split('=')[1];
-    token.should.not.equal('undefined');
-    token.length.should.be.above(10);
+      flickrClient.authApp(callbackURL, function(settings){
+        settings.should.have.properties('oauth_callback_confirmed', 'oauth_token', 'oauth_token_secret');
+        settings.oauth_callback_confirmed.should.equal('true');
 
-    // Make the auth url available so we can later verify it works
-    this.authURL = url;
-  });
-
-
-  it('should be able to fetch a user access token', function(done){
-    this.timeout(30000);
-
-    var matched = function(queryParams){
-      flickrClient.getUserAccessToken(queryParams.oauth_verifier, function(accessToken){
-        accessToken.should.have.properties('oauth_token', 'oauth_token_secret', 'user_nsid', 'username');
-        accessToken.oauth_token_secret.should.not.equal('undefined');
-        accessToken.oauth_token_secret.should.not.equal('');
-        userToken = accessToken;
         done();
       });
-    };
+    });
 
-    authUtils.createRouteListener('/auth_callback', matched);
-    authUtils.simulateUserApproval(flickrClient.getUserAuthURL());
+
+    it('should return a user authorization URL when prompted', function(){
+      this.timeout(5000);
+
+      var url   = flickrClient.getUserAuthURL();
+      var query = url.split('?')[1];
+      var token = query.split('=')[1];
+      token.should.not.equal('undefined');
+      token.length.should.be.above(10);
+
+      // Make the auth url available so we can later verify it works
+      this.authURL = url;
+    });
+
+
+    it('should be able to fetch a user access token', function(done){
+      this.timeout(30000);
+
+      var matched = function(queryParams){
+        flickrClient.getUserAccessToken(queryParams.oauth_verifier, function(accessToken){
+          accessToken.should.have.properties('oauth_token', 'oauth_token_secret', 'user_nsid', 'username');
+          accessToken.oauth_token_secret.should.not.equal('undefined');
+          accessToken.oauth_token_secret.should.not.equal('');
+          userToken = accessToken;
+          done();
+        });
+      };
+
+      authUtils.createRouteListener('/auth_callback', matched);
+      authUtils.simulateUserApproval(flickrClient.getUserAuthURL());
+    });
+
   });
-
-});
+}
 
 
 exports.getUserAccessToken = function(){
-  return userToken
+  if(cli.useAuth) return userToken
+  else return {
+    fullname            : 'First Last',
+    oauth_token         : process.env.FLICKR_OAUTH_USER_TOKEN,
+    oauth_token_secret  : process.env.FLICKR_OAUTH_USER_SECRET,
+    user_nsid           : process.env.FLICKR_NSID,
+    username            : process.env.FLICKR_USERNAME
+  }
 };
--- test/client.js
+++ test/client.js
@@ -1,14 +1,24 @@
 var should        = require('should');
 var flickr        = require('../index');
+var cli           = require('./cli');
 var flickrClient  = new flickr({
   oauth_consumer_key    : process.env.FLICKR_KEY,
   oauth_consumer_secret : process.env.FLICKR_SECRET,
   perms                 : 'delete'
 });
 
+if(!cli.useAuth){
+  flickrClient.settings = {
+    oauth_consumer_key    : process.env.FLICKR_KEY,
+    oauth_consumer_secret : process.env.FLICKR_SECRET,
+    oauth_token           : process.env.FLICKR_OAUTH_TOKEN,
+    oauth_token_secret    : process.env.FLICKR_OAUTH_SECRET,
+    perms                 : 'delete'
+  };
+}
+
 
 describe('initialization', function(){
-
 
   it('should return all settings defined', function(){
     flickrClient.settings.should.have.property('oauth_consumer_key');
